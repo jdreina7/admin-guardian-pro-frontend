75fae003704882960f2a722daabbeda1a05a88bc
Juan David Reina <judareiro@gmail.com>
2024-02-24 23:31:27 -0500

    AGP-52: Implementing new style for the login

--- src/app/main/sign-in/SignInPage.tsx
+++ src/app/main/sign-in/SignInPage.tsx
@@ -1,247 +1,173 @@
-import Typography from '@mui/material/Typography';
+import { Controller, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { useState } from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import CardContent from '@mui/material/CardContent';
 import _ from '@lodash';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import JwtLoginTab from './tabs/JwtSignInTab';
-import FirebaseSignInTab from './tabs/FirebaseSignInTab';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Box, Button, Checkbox, FormControl, FormControlLabel, Paper, TextField, Typography } from '@mui/material';
+import sample from 'public/assets/images/videos/login-video.mp4';
 
-const tabs = [
-    {
-        id: 'jwt',
-        title: 'JWT',
-        logo: 'assets/images/logo/jwt.svg',
-        logoClass: 'h-40 p-4 bg-black rounded-12'
-    },
-    {
-        id: 'firebase',
-        title: 'Firebase',
-        logo: 'assets/images/logo/firebase.svg',
-        logoClass: 'h-40'
-    }
-];
+/**
+ * Form Validation Schema
+ */
+const schema = z.object({
+    email: z.string().email('You must enter a valid email').nonempty('You must enter an email'),
+    password: z
+        .string()
+        .min(6, 'Password is too short - must be at least 6 chars.')
+        .nonempty('Please enter your password.')
+});
+
+type FormType = {
+    email: string;
+    password: string;
+    remember?: boolean;
+};
+
+const defaultValues = {
+    email: '',
+    password: '',
+    remember: true
+};
 
 /**
  * The sign in page.
  */
 function SignInPage() {
-    const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+    const { control, formState, handleSubmit, reset } = useForm<FormType>({
+        mode: 'onChange',
+        defaultValues,
+        resolver: zodResolver(schema)
+    });
 
-    function handleSelectTab(id: string) {
-        setSelectedTabId(id);
+    const { isValid, dirtyFields, errors } = formState;
+
+    function onSubmit() {
+        reset(defaultValues);
     }
 
     return (
-        <div className="flex min-w-0 flex-1 flex-col items-center sm:flex-row sm:justify-center md:items-start md:justify-start">
-            <Paper className="h-full w-full px-16 py-8 ltr:border-r-1 rtl:border-l-1 sm:h-auto sm:w-auto sm:rounded-2xl sm:p-48 sm:shadow md:flex md:h-full md:w-1/2 md:items-center md:justify-end md:rounded-none md:p-64 md:shadow-none">
-                <CardContent className="mx-auto w-full max-w-320 sm:mx-0 sm:w-320">
-                    <img
-                        className="w-48"
-                        src="assets/images/logo/logo.svg"
-                        alt="logo"
-                    />
-
-                    <Typography className="mt-32 text-4xl font-extrabold leading-tight tracking-tight">
-                        Sign in
-                    </Typography>
-                    <div className="mt-2 flex items-baseline font-medium">
-                        <Typography>Don't have an account?</Typography>
-                        <Link
-                            className="ml-4"
-                            to="/sign-up"
-                        >
-                            Sign up
-                        </Link>
-                    </div>
-
-                    <Alert
-                        icon={false}
-                        severity="info"
-                        className="mt-24 px-16 text-13 leading-relaxed"
-                    >
-                        You are browsing <b>Fuse React Demo</b>. Click on the "Sign in" button to access the Demo and
-                        Documentation.
-                    </Alert>
-
-                    <Tabs
-                        value={_.findIndex(tabs, { id: selectedTabId })}
-                        variant="fullWidth"
-                        className="w-full mt-24 mb-32"
-                        indicatorColor="secondary"
-                    >
-                        {tabs.map((item) => (
-                            <Tab
-                                onClick={() => handleSelectTab(item.id)}
-                                key={item.id}
-                                icon={
-                                    <img
-                                        className={item.logoClass}
-                                        src={item.logo}
-                                        alt={item.title}
-                                    />
-                                }
-                                className="min-w-0"
-                                label={item.title}
-                            />
-                        ))}
-                    </Tabs>
-
-                    {selectedTabId === 'jwt' && <JwtLoginTab />}
-                    {selectedTabId === 'firebase' && <FirebaseSignInTab />}
-
-                    <div className="mt-32 flex items-center">
-                        <div className="mt-px flex-auto border-t" />
-                        <Typography
-                            className="mx-8"
-                            color="text.secondary"
-                        >
-                            Or continue with
-                        </Typography>
-                        <div className="mt-px flex-auto border-t" />
-                    </div>
-
-                    <div className="mt-32 flex items-center space-x-16">
-                        <Button
-                            variant="outlined"
-                            className="flex-auto"
-                        >
-                            <FuseSvgIcon
-                                size={20}
-                                color="action"
-                            >
-                                feather:facebook
-                            </FuseSvgIcon>
-                        </Button>
-                        <Button
-                            variant="outlined"
-                            className="flex-auto"
-                        >
-                            <FuseSvgIcon
-                                size={20}
-                                color="action"
-                            >
-                                feather:twitter
-                            </FuseSvgIcon>
-                        </Button>
-                        <Button
-                            variant="outlined"
-                            className="flex-auto"
-                        >
-                            <FuseSvgIcon
-                                size={20}
-                                color="action"
-                            >
-                                feather:github
-                            </FuseSvgIcon>
-                        </Button>
-                    </div>
-                </CardContent>
-            </Paper>
-
-            <Box
-                className="relative hidden h-full flex-auto items-center justify-center overflow-hidden p-64 md:flex lg:px-112"
-                sx={{ backgroundColor: 'primary.main' }}
+        <div className="flex min-w-0 flex-auto flex-col items-center sm:justify-center md:p-32">
+            <video
+                className="fixed w-full z-[-1]"
+                style={{ filter: 'blur(3px)' }}
+                autoPlay
+                loop
+                muted
             >
-                <svg
-                    className="pointer-events-none absolute inset-0"
-                    viewBox="0 0 960 540"
-                    width="100%"
-                    height="100%"
-                    preserveAspectRatio="xMidYMax slice"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <Box
-                        component="g"
-                        sx={{ color: 'primary.light' }}
-                        className="opacity-20"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="100"
-                    >
-                        <circle
-                            r="234"
-                            cx="196"
-                            cy="23"
-                        />
-                        <circle
-                            r="234"
-                            cx="790"
-                            cy="491"
-                        />
-                    </Box>
-                </svg>
+                <source
+                    src={sample}
+                    type="video/mp4"
+                />
+            </video>
+            <Paper className="flex min-h-full w-full overflow-hidden rounded-0 sm:min-h-auto sm:w-auto sm:rounded-2xl sm:shadow md:max-w-6xl">
                 <Box
-                    component="svg"
-                    className="absolute -right-64 -top-64 opacity-20"
-                    sx={{ color: 'primary.light' }}
-                    viewBox="0 0 220 192"
-                    width="220px"
-                    height="192px"
-                    fill="none"
+                    className="relative hidden h-full flex-auto items-center justify-center overflow-hidden md:flex"
+                    // sx={{ backgroundColor: 'primary.main' }}
                 >
-                    <defs>
-                        <pattern
-                            id="837c3e70-6c3a-44e6-8854-cc48c737b659"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                fill="currentColor"
-                            />
-                        </pattern>
-                    </defs>
-                    <rect
-                        width="220"
-                        height="192"
-                        fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                    <img
+                        className="w-3/5"
+                        src="assets/images/logo/logo512.svg"
+                        alt="logo"
                     />
                 </Box>
 
-                <div className="relative z-10 w-full max-w-2xl">
-                    <div className="text-7xl font-bold leading-none text-gray-100">
-                        <div>Welcome to</div>
-                        <div>our community</div>
-                    </div>
-                    <div className="mt-24 text-lg leading-6 tracking-tight text-gray-400">
-                        Fuse helps developers to build organized and well coded dashboards full of beautiful and rich
-                        modules. Join us and start building your application today.
-                    </div>
-                    <div className="mt-32 flex items-center">
-                        <AvatarGroup
-                            sx={{
-                                '& .MuiAvatar-root': {
-                                    borderColor: 'primary.main'
-                                }
-                            }}
-                        >
-                            <Avatar src="assets/images/avatars/female-18.jpg" />
-                            <Avatar src="assets/images/avatars/female-11.jpg" />
-                            <Avatar src="assets/images/avatars/male-09.jpg" />
-                            <Avatar src="assets/images/avatars/male-16.jpg" />
-                        </AvatarGroup>
+                <div className="w-full px-16 py-32 ltr:border-l-1 rtl:border-r-1 sm:w-auto sm:p-48 md:p-64">
+                    <div className="mx-auto w-full max-w-320 sm:mx-0 sm:w-320">
+                        <img
+                            className="m-auto md:hidden sm:block xs:block"
+                            src="assets/images/logo/logo192.svg"
+                            alt="logo"
+                        />
 
-                        <div className="ml-16 font-medium tracking-tight text-gray-400">
-                            More than 17k people joined us, it's your turn
-                        </div>
+                        <Typography className="mt-32 text-4xl font-extrabold leading-tight tracking-tight">
+                            Sign in
+                        </Typography>
+
+                        <form
+                            name="loginForm"
+                            noValidate
+                            className="mt-32 flex w-full flex-col justify-center"
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            <Controller
+                                name="email"
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        className="mb-24"
+                                        label="Email"
+                                        autoFocus
+                                        type="email"
+                                        error={!!errors.email}
+                                        helperText={errors?.email?.message}
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                    />
+                                )}
+                            />
+
+                            <Controller
+                                name="password"
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        className="mb-24"
+                                        label="Password"
+                                        type="password"
+                                        error={!!errors.password}
+                                        helperText={errors?.password?.message}
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                    />
+                                )}
+                            />
+
+                            <div className="flex flex-col items-center justify-center sm:flex-row sm:justify-between">
+                                <Controller
+                                    name="remember"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <FormControl>
+                                            <FormControlLabel
+                                                label="Remember me"
+                                                control={
+                                                    <Checkbox
+                                                        size="small"
+                                                        {...field}
+                                                    />
+                                                }
+                                            />
+                                        </FormControl>
+                                    )}
+                                />
+
+                                <Link
+                                    className="text-md font-medium"
+                                    to="/pages/auth/forgot-password"
+                                >
+                                    Forgot password?
+                                </Link>
+                            </div>
+
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                className=" mt-16 w-full"
+                                aria-label="Sign in"
+                                disabled={_.isEmpty(dirtyFields) || !isValid}
+                                type="submit"
+                                size="large"
+                            >
+                                Sign in
+                            </Button>
+                        </form>
                     </div>
                 </div>
-            </Box>
+            </Paper>
         </div>
     );
 }
--- src/app/theme-layouts/themeLayouts.ts
+++ src/app/theme-layouts/themeLayouts.ts
@@ -7,16 +7,16 @@ import Layout3 from './layout3/Layout3';
  * The type definition for the theme layouts.
  */
 export type themeLayoutsType = {
-	[key: string]: ComponentType<{ children?: React.ReactNode }>;
+    [key: string]: ComponentType<{ children?: React.ReactNode }>;
 };
 
 /**
  * The theme layouts.
  */
 const themeLayouts: themeLayoutsType = {
-	layout1: Layout1,
-	layout2: Layout2,
-	layout3: Layout3
+    layout1: Layout1,
+    layout2: Layout2,
+    layout3: Layout3
 };
 
 export default themeLayouts;
