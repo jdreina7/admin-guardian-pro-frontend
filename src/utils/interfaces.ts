6c1916519f87d33d084017ce3ae44c8977e6a90f
Juan David Reina <judareiro@gmail.com>
2024-04-23 23:41:12 -0500

    AGP-54: Solved the user data persistance issue on the browser after the loged user reload the browser

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,3 +2,29 @@ export interface IAPIErrorResponse {
     message: string;
     success: boolean;
 }
+
+export interface IIdentificationType {
+    type: string;
+    id: string;
+}
+
+export interface IGenericInterface {
+    name: string;
+    id: string;
+}
+
+export interface ILayout {
+    style: string;
+}
+
+export interface ITheme {
+    main: string;
+    navbar: string;
+    toolbar: string;
+    footer: string;
+}
+export interface ISettings {
+    layout: ILayout;
+    theme: ITheme;
+    _id: string;
+}
