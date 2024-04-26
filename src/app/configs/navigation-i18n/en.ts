2b6db63420dc77d5970d8699697ad50c44cf5cff
Juan David Reina <judareiro@gmail.com>
2024-04-25 21:41:41 -0500

    AGP-49: Adding the dashboard page, and show the logged user role

--- src/app/configs/navigation-i18n/en.ts
+++ src/app/configs/navigation-i18n/en.ts
@@ -1,6 +1,8 @@
 const locale = {
-	APPLICATIONS: 'Applications',
-	EXAMPLE: 'Example'
+    APPLICATIONS: 'Applications',
+    EXAMPLE: 'Example',
+    // Dashboard
+    DASHBOARD: 'Dashboard'
 };
 
 export default locale;
---
+++ src/app/main/dashboard/Dashboard.tsx
@@ -0,0 +1,40 @@
+import FusePageSimple from '@fuse/core/FusePageSimple';
+import { styled } from '@mui/material/styles';
+import FuseLoading from '@fuse/core/FuseLoading';
+
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../auth/user/store/userSlice';
+import DashboardHeader from './DashboardHeader';
+
+const Root = styled(FusePageSimple)(({ theme }) => ({
+    '& .FusePageSimple-header': {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: `inset 0 0 0 1px  ${theme.palette.divider}`
+    }
+}));
+
+/**
+ * The Dashboard page.
+ */
+function Dashboard() {
+    const user = useSelector(selectUser);
+
+    setTimeout(() => {
+        return <FuseLoading />;
+    }, 2000);
+
+    return (
+        <Root
+            header={<DashboardHeader />}
+            content={
+                <div className="w-full p-12 pt-16 sm:pt-24 lg:ltr:pr-0 lg:rtl:pl-0">
+                    <p>
+                        Hello <b>{user?.role}</b> user!
+                    </p>
+                </div>
+            }
+        />
+    );
+}
+
+export default Dashboard;
