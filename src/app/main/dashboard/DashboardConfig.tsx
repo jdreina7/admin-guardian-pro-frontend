ce2f0fe7068da97efe46cb8c4b4bd3bc9a1c345d
Giancarlo Reina <giancarloreina125@gmail.com>
2024-05-18 01:04:22 -0500

    AGP-58: La bandera del idioma Español fue añadida y las otras dos (Turquia-Arabe) fueron eliminadas

--- src/app/main/dashboard/DashboardConfig.tsx
+++ src/app/main/dashboard/DashboardConfig.tsx
@@ -1,7 +1,11 @@
 import { lazy } from 'react';
+import i18next from 'i18next';
+
+
 
 const Dashboard = lazy(() => import('./Dashboard'));
 
+i18next.addResourceBundle('en', 'mailboxApp', en);
 /**
  * The Dashboard configuration.
  */
